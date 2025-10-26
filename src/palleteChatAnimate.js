a6edc18c750f0cb6a17cd98cf535d1857119edd9
amirhosseinmz <45499633+amirhosseinmz@users.noreply.github.com>
2025-10-26 11:46:17 +0330

    Add files via upload

--- src/palleteChatAnimate.js
+++ src/palleteChatAnimate.js
@@ -33,7 +33,7 @@ var palleteChatAnimate = function(url_repository,id){
     };
     this.fetchData = (callback)=>{
         if(this.repositoryData){
-            this.urlChat = this.repositoryData[parseInt(Math.random()*this.repositoryData.length)];
+            this.urlChat = this.repositoryData[parseInt(Math.random()*(this.repositoryData.length+1))];
             if(this.cache[this.urlChat]){
                 this.chatData = this.cache[this.urlChat];
                 callback(); 
